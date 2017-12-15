fc8432a8f8f8fbbc35c6d076675284df8f26e8f9
Adrian Scott <34492594+DaToasty@users.noreply.github.com>
2017-12-15 16:21:48 +1000

    Create wipe.php ( Wipe website script )

---
+++ installer/wipe.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "PHP-forum v0.1 - Wipe website script<br>";
+
+$database = include('../conf/database.php');
+$conn = new mysqli($database['host'], $database['user'], $database['pass'], "phpf");
+
+$sql = "DROP DATABASE phpf";
+
+if (mysqli_query($conn, $sql)) {
+   echo "Website has been wiped<br>";
+   mysqli_close($conn);
+   die("Thanks for using PHP-forum!");
+} else {
+   echo "Error wiping website: " . mysqli_error($conn) . "<br>";
+   die("Wipe stopped");
+}
+?>
