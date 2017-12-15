7269dfe9c76606e947f3ad5cdf24eddd140782ac
Adrian Scott <34492594+DaToasty@users.noreply.github.com>
2017-12-16 00:21:56 +1000

    Create Default/index.php

---
+++ styles/Default/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+    <title>Homepage | <?php echo $wname["sitename"] ?></title>
+
+<head>
+
+</head>
+<body>
+
+    <a href="#">Homepage</a>
+    <h1><?php echo $wname["sitename"] ?></h1>
+
+</body>
+</html>
